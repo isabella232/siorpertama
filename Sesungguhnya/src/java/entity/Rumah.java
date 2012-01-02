c80f0c60611aa97bfff4df9e8c4fa9ac3b32ec81
meylindradevi@gmail.com <meylindradevi@gmail.com@b4413743-58a3-4b35-ae52-c60928a3cf02>
2012-01-02 11:07:31 +0000

    Entity Rumah

--- Sesungguhnya/src/java/entity/Rumah.java
+++ Sesungguhnya/src/java/entity/Rumah.java
@@ -16,108 +16,94 @@ import javax.persistence.Id;
  */
 @Entity
 public class Rumah implements Serializable {
-    private static final long serialVersionUID = 1L;
+     private static final long serialVersionUID = 1L;
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private Integer idRum;
-    private String alamatRumah;
-    private int rtRumah;
-    private int rwRumah;
-    private String kelurahanRt;
-    private String kecamatanRumah;
-    private String kotaRumah;
-    private String provinsiRumah;
+    private Integer idrumah; 
+    private int rt;
+    private int rw;
+    private String Alamat;
+    private String kelurahan;
     private String kodeRumah;
-
-    public Integer getIdRum() {
-        return idRum;
-    }
-
-    public void setIdRum(Integer idRum) {
-        this.idRum = idRum;
-    }
-
-    public String getAlamatRumah() {
-        return alamatRumah;
-    }
-
-    public void setAlamatRumah(String alamatRumah) {
-        this.alamatRumah = alamatRumah;
-    }
-
-    public int getRtRumah() {
-        return rtRumah;
-    }
-
-    public void setRtRumah(int rtRumah) {
-        this.rtRumah = rtRumah;
-    }
-
-    public int getRwRumah() {
-        return rwRumah;
-    }
-
-    public void setRwRumah(int rwRumah) {
-        this.rwRumah = rwRumah;
-    }
-
-    public String getKelurahanRt() {
-        return kelurahanRt;
-    }
-
-    public void setKelurahanRt(String kelurahanRt) {
-        this.kelurahanRt = kelurahanRt;
-    }
-
-    public String getKecamatanRumah() {
-        return kecamatanRumah;
-    }
-
-    public void setKecamatanRumah(String kecamatanRumah) {
-        this.kecamatanRumah = kecamatanRumah;
-    }
-
-    public String getKotaRumah() {
-        return kotaRumah;
-    }
-
-    public void setKotaRumah(String kotaRumah) {
-        this.kotaRumah = kotaRumah;
-    }
-
-    public String getProvinsiRumah() {
-        return provinsiRumah;
-    }
-
-    public void setProvinsiRumah(String provinsiRumah) {
-        this.provinsiRumah = provinsiRumah;
-    }
+    private String kecamatan;
+    private String kota;
+    private String provinsi;
     
-    public String getKodeRumah() {
-        return provinsiRumah;
+
+    public Integer getIdrum() {
+        return idrumah;
     }
 
-    public void setKodeRumah(String kodeRumah) {
+    public void setIdrum(Integer idrumah) {
+        this.idrumah = idrumah;
+    }
+
+    public String getKoderum() {
+        return kodeRumah;
+    }
+    public int getRT(){
+       return rt; 
+    } 
+    public void setRT(int rt){
+        this.rt=rt;
+    }
+    public int getRW(){{
+        return rw;
+    }
+   public void setRW(int rw){
+        this.rw=rw;
+    }
+    public void setKodekel(String kodeRumah) {
         this.kodeRumah = kodeRumah;
     }
-    
-    
+
+    public String getAlamat() {
+        return Alamat;
+    }
+
+    public void setAlamat(String Alamat) {
+        this.Alamat = Alamat;
+    }
+
+    public String getKelurahan() {
+        return kelurahan;
+    }
+    public void setKelurahan(String kelurahan){
+        this.kelurahan=kelurahan;
+    }
+    public String getKecamatan(){
+        return kecamatan;
+    }
+    public void setKecamatan(String kecamatan){
+        this.kecamatan=kecamatan;
+    }
+    public String getKota(){
+        return kota;
+    }
+    public void setKota(String kota){
+        this.kota=kota;
+    }
+    public String getProvinsi(){
+      return provinsi;  
+    }
+    public void setPovinsi(String provinsi) {
+        this.provinsi=provinsi;
+    }
 
     @Override
     public int hashCode() {
         int hash = 0;
-        hash += (idRum != null ? idRum.hashCode() : 0);
+        hash += (idrumah != null ? idrumah.hashCode() : 0);
         return hash;
     }
 
     @Override
     public boolean equals(Object object) {
-        // TODO: Warning - this method won't work in the case the id fields are not set
         if (!(object instanceof Rumah)) {
             return false;
         }
         Rumah other = (Rumah) object;
-        if ((this.idRum == null && other.idRum != null) || (this.idRum != null && !this.idRum.equals(other.idRum))) {
+        if ((this.idrumah == null && other.idrumah != null) || (this.idrumah != null && !this.idrumah.equals(other.idrumah))) {
             return false;
         }
         return true;
@@ -130,5 +116,5 @@ public class Rumah implements Serializable {
             return false;
         }
     }
-    
+
 }
