c6ce1b2c484f3be612099c13106adca8c55d9c6b
meylindradevi@gmail.com <meylindradevi@gmail.com@b4413743-58a3-4b35-ae52-c60928a3cf02>
2012-01-14 05:23:10 +0000

    update terbaru lagi

--- bismillah/src/java/entity/Rumah.java
+++ bismillah/src/java/entity/Rumah.java
@@ -5,139 +5,120 @@
 package entity;
 
 import java.io.Serializable;
-import javax.persistence.Basic;
-import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.NamedQueries;
-import javax.persistence.NamedQuery;
-import javax.persistence.Table;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
-import javax.xml.bind.annotation.XmlRootElement;
 
 /**
  *
- * @author TYA
+ * @author Meylindra Ap
  */
 @Entity
-@Table(name = "rumah")
-@XmlRootElement
-@NamedQueries({
-    @NamedQuery(name = "Rumah.findAll", query = "SELECT r FROM Rumah r"),
-    @NamedQuery(name = "Rumah.findByKoderumah", query = "SELECT r FROM Rumah r WHERE r.koderumah = :koderumah"),
-    @NamedQuery(name = "Rumah.findByAlamatrumah", query = "SELECT r FROM Rumah r WHERE r.alamatrumah = :alamatrumah"),
-    @NamedQuery(name = "Rumah.findByKelurahanrumah", query = "SELECT r FROM Rumah r WHERE r.kelurahanrumah = :kelurahanrumah"),
-    @NamedQuery(name = "Rumah.findByKecamatanrumah", query = "SELECT r FROM Rumah r WHERE r.kecamatanrumah = :kecamatanrumah"),
-    @NamedQuery(name = "Rumah.findByKotarumah", query = "SELECT r FROM Rumah r WHERE r.kotarumah = :kotarumah")})
-
 public class Rumah implements Serializable {
-    private static final long serialVersionUID = 1L;
+     private static final long serialVersionUID = 1L;
     @Id
-    @Basic(optional = false)
-    @NotNull
-    @Size(min = 1, max = 10)
-    @Column(name = "koderumah")
-    private String koderumah;
-    @Basic(optional = false)
-    @NotNull
-    @Size(min = 1, max = 50)
-    @Column(name = "alamatrumah")
-    private String alamatrumah;
-    @Basic(optional = false)
-    @NotNull
-    @Size(min = 1, max = 30)
-    @Column(name = "kelurahanrumah")
-    private String kelurahanrumah;
-    @Basic(optional = false)
-    @NotNull
-    @Size(min = 1, max = 20)
-    @Column(name = "kecamatanrumah")
-    private String kecamatanrumah;
-    @Basic(optional = false)
-    @NotNull
-    @Size(min = 1, max = 25)
-    @Column(name = "kotarumah")
-    private String kotarumah;
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer idrumah; 
+    private int rt;
+    private int rw;
+    private String Alamat;
+    private String kelurahan;
+    private String kodeRumah;
+    private String kecamatan;
+    private String kota;
+    private String provinsi;
+    
 
-    public Rumah() {
+    public Integer getIdrum() {
+        return idrumah;
     }
 
-    public Rumah(String koderumah) {
-        this.koderumah = koderumah;
+    public void setIdrum(Integer idrumah) {
+        this.idrumah = idrumah;
     }
 
-    public Rumah(String koderumah, String alamatrumah, String kelurahanrumah, String kecamatanrumah, String kotarumah) {
-        this.koderumah = koderumah;
-        this.alamatrumah = alamatrumah;
-        this.kelurahanrumah = kelurahanrumah;
-        this.kecamatanrumah = kecamatanrumah;
-        this.kotarumah = kotarumah;
+    public String getKoderum() {
+        return kodeRumah;
+    }
+    public int getRT(){
+       return rt; 
+    } 
+    public void setRT(int rt){
+        this.rt=rt;
+    }
+    public int getRW(){
+        return rw;
+    }
+   public void setRW(int rw){
+        this.rw=rw;
+    }
+    public void setKodeRumah(String kodeRumah) {
+        this.kodeRumah = kodeRumah;
     }
 
-    public String getKoderumah() {
-        return koderumah;
+    public String getAlamat() {
+        return Alamat;
     }
 
-    public void setKoderumah(String koderumah) {
-        this.koderumah = koderumah;
+    public void setAlamat(String Alamat) {
+        this.Alamat = Alamat;
     }
 
-    public String getAlamatrumah() {
-        return alamatrumah;
+    public String getKelurahan() {
+        return kelurahan;
     }
-
-    public void setAlamatrumah(String alamatrumah) {
-        this.alamatrumah = alamatrumah;
+    public void setKelurahan(String kelurahan){
+        this.kelurahan=kelurahan;
     }
-
-    public String getKelurahanrumah() {
-        return kelurahanrumah;
+    public String getKecamatan(){
+        return kecamatan;
     }
-
-    public void setKelurahanrumah(String kelurahanrumah) {
-        this.kelurahanrumah = kelurahanrumah;
+    public void setKecamatan(String kecamatan){
+        this.kecamatan=kecamatan;
     }
-
-    public String getKecamatanrumah() {
-        return kecamatanrumah;
+    public String getKota(){
+        return kota;
     }
-
-    public void setKecamatanrumah(String kecamatanrumah) {
-        this.kecamatanrumah = kecamatanrumah;
+    public void setKota(String kota){
+        this.kota=kota;
     }
-
-    public String getKotarumah() {
-        return kotarumah;
+    public String getProvinsi(){
+      return provinsi;  
     }
-
-    public void setKotarumah(String kotarumah) {
-        this.kotarumah = kotarumah;
+    public void setPovinsi(String provinsi) {
+        this.provinsi=provinsi;
     }
 
     @Override
     public int hashCode() {
         int hash = 0;
-        hash += (koderumah != null ? koderumah.hashCode() : 0);
+        hash += (idrumah != null ? idrumah.hashCode() : 0);
         return hash;
     }
 
     @Override
     public boolean equals(Object object) {
-        // TODO: Warning - this method won't work in the case the id fields are not set
         if (!(object instanceof Rumah)) {
             return false;
         }
         Rumah other = (Rumah) object;
-        if ((this.koderumah == null && other.koderumah != null) || (this.koderumah != null && !this.koderumah.equals(other.koderumah))) {
+        if ((this.idrumah == null && other.idrumah != null) || (this.idrumah != null && !this.idrumah.equals(other.idrumah))) {
             return false;
         }
         return true;
     }
 
-    @Override
-    public String toString() {
-        return "entity.Rumah[ koderumah=" + koderumah + " ]";
+     public boolean isKodeNoChange(String kode) {
+        if (kode.equalsIgnoreCase(this.kodeRumah)) {
+            return true;
+        } else {
+            return false;
+        }
     }
-    
+
+    public void setAlamatRumah(String almtrumah) {
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
 }
